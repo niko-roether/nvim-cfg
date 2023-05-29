36ae10543077f8d1a0d119dec7113ee2b7578369
Nicholas Roether <nicholas.roether@t-online.de>
2023-05-30 00:27:03 +0200

    Switch leader from . to ,

--- lua/niko/remaps.lua
+++ lua/niko/remaps.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = "."
+vim.g.mapleader = ","
 
 -- Explore files
 vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
@@ -6,8 +6,8 @@ vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
 -- Interact with tabs
 vim.keymap.set("n", "<C-T>", function() vim.cmd.tabnew(".") end)
 vim.keymap.set("n", "<C-W>", vim.cmd.tabclose)
+vim.keymap.set("n", "<C-H>", function() vim.cmd.tabnext("-") end)
 vim.keymap.set("n", "<C-L>", function() vim.cmd.tabnext("+") end)
-vim.keymap.set("n", "<C-J>", function() vim.cmd.tabnext("-") end)
 
 -- Git
 vim.keymap.set("n", "<leader>g", vim.cmd.Git)
