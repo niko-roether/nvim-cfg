afbfe37182a4d98d0cc0335299162543301f1642
Nicholas Roether <nicholas.roether@t-online.de>
2023-06-01 00:18:03 +0200

    Add keybindings for working with floaterm

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,2 +1,6 @@
-vim.keymap.set("n", "<leader>t", vim.cmd.FloatermToggle)
-vim.keymap.set("t", "<ESC>", vim.cmd.FloatermToggle)
+vim.keymap.set("n", "<C-T>", vim.cmd.FloatermToggle)
+vim.keymap.set("t", "<C-T>", vim.cmd.FloatermHide)
+vim.keymap.set("t", "<C-Q>", vim.cmd.FloatermKill)
+vim.keymap.set("t", "<C-N>", vim.cmd.FloatermNew)
+vim.keymap.set("t", "<C-H>", vim.cmd.FloatermPrev)
+vim.keymap.set("t", "<C-L>", vim.cmd.FloatermNext)
