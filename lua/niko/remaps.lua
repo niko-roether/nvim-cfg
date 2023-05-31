7fbce933b8a0970a32968873b6b72fe20a8e12de
nicholasroether4308 <nicholas.roether@otto.de>
2023-05-31 23:38:01 +0200

    Replace tab shortcuts with split screen shortcuts

--- lua/niko/remaps.lua
+++ lua/niko/remaps.lua
@@ -3,14 +3,13 @@ vim.g.mapleader = ","
 -- Explore files
 vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
 
--- Interact with tabs
-vim.keymap.set("n", "<C-T>", function() vim.cmd.tabnew(".") end)
-vim.keymap.set("n", "<C-W>", vim.cmd.tabclose)
-vim.keymap.set("n", "<C-H>", function() vim.cmd.tabnext("-") end)
-vim.keymap.set("n", "<C-L>", function() vim.cmd.tabnext("+") end)
-
 -- Git
 vim.keymap.set("n", "<leader>g", vim.cmd.Git)
 
 -- Mode switching
 vim.keymap.set("i", "<C-Enter>", "<Esc>")
+
+-- Split-Screen
+vim.keymap.set("n", "<leader>sv", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>sh", vim.cmd.split)
+
