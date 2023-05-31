520c8b38fb3e5e4c305e6549b1b6e6f13c007a45
Nicholas Roether <nicholas.roether@t-online.de>
2023-06-01 00:17:44 +0200

    Add show type keybind

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,3 +20,9 @@ lsp.setup_nvim_cmp {
 }
 
 lsp.setup()
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+	end
+})
