e96ff240a6f9a757e9ee836f4cfa34241f3e38c1
Nicholas Roether <nicholas.roether@t-online.de>
2023-06-26 01:58:07 +0200

    Change fugitive keybind to not interfere witch gd/gr

--- lua/niko/remaps.lua
+++ lua/niko/remaps.lua
@@ -4,7 +4,7 @@ vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
 
 -- Git
-vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+vim.keymap.set("n", "<C-G>", vim.cmd.Git)
 
 -- Mode switching
 vim.keymap.set("i", "<C-Enter>", "<Esc>")
@@ -12,4 +12,3 @@ vim.keymap.set("i", "<C-Enter>", "<Esc>")
 -- Split-Screen
 vim.keymap.set("n", "<leader>sv", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>sh", vim.cmd.split)
-
