3f41f400929d68e30002924ff150852f2c75ae7e
Nicholas Roether <nicholas.roether@t-online.de>
2023-09-18 21:52:53 +0200

    Proper prettier + eslint support

--- lua/lsp/config/efm.lua
+++ lua/lsp/config/efm.lua
@@ -3,19 +3,39 @@ local eslint_d = {
 	lintStdin = true,
 	lintFormats = { "%f:%l:%c: %tarning %m", "%f:%l:%c: %trror %m" },
 	lintIgnoreExitCode = true,
-	lintSource = "eslint"
+	lintSource = "eslint",
+	formatCommand = "eslint_d --stdin --fix-to-stdout --stdin-filename=${INPUT}]",
+	formatStdin = true
+}
+
+local prettierd = {
+	formatCommand = "prettierd ${INPUT}",
+	formatStdin = true
 }
 
 local function config(opts)
-	opts = {}
+	function opts.on_attach(client)
+		client.server_capabilities.documentFormattingProvider = true
+	end
 
 	opts.settings = {
 		rootMarkers = { ".git", "package.json" },
 		languages = {
-			javascript = { eslint_d },
-			javascriptreact = { eslint_d },
-			typescript = { eslint_d },
-			typescriptreact = { eslint_d }
+			javascript = { eslint_d, prettierd },
+			javascriptreact = { eslint_d, prettierd },
+			typescript = { eslint_d, prettierd },
+			typescriptreact = { eslint_d, prettierd },
+			yaml = { prettierd },
+			markdown = { prettierd },
+			css = { prettierd },
+			scss = { prettierd },
+			sass = { prettierd },
+			less = { prettierd },
+			json = { prettierd },
+			graphql = { prettierd },
+			vue = { prettierd },
+			html = { prettierd },
+			svelte = { prettierd }
 		}
 	}
 
