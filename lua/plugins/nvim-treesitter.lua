8201162861ff3572bc6d4a49866bbaf166ab9793
nicholas-roether <nicholas.roether@t-online.de>
2023-09-19 10:38:21 +0200

    Get treesitter to work (ffs)

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,21 @@
 return {
-	{ "nvim-treesitter" }
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = function()
+		vim.cmd.TSUpdate()
+	end,
+	opts = {
+		auto_install = true,
+		sync_install = false,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false
+		},
+		autotag = {
+			enable = true
+		}
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end
 }
