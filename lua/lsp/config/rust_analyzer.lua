8ff2ea07c4fba29df19ab67aebb8e14c5121c761
nicholas-roether <nicholas.roether@t-online.de>
2023-10-10 14:46:27 +0200

    Add leptosfmt support

--- lua/lsp/config/rust_analyzer.lua
+++ lua/lsp/config/rust_analyzer.lua
@@ -1,8 +1,23 @@
 local function config(opts)
+	local leptosfmt_exists = vim.fs.find("leptosfmt.toml", {
+		upward = true,
+		stop = "/",
+		path = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+	}) ~= nil;
+
+	local rustfmt_override = nil
+
+	if leptosfmt_exists then
+		rustfmt_override = { "leptosfmt", "--stdin", "--rustfmt" }
+	end
+
 	opts.settings = {
 		["rust-analyzer"] = {
 			checkOnSave = {
 				command = "clippy"
+			},
+			rustfmt = {
+				overrideCommand = rustfmt_override
 			}
 		}
 	}
