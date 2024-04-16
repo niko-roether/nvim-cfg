12f3b3d7867c7cf0ea935e5d6ff78d41a5414e16
Nicholas Roether <nicholas.roether@t-online.de>
2024-04-16 23:51:00 +0200

    Use /bin/sh as a shell
    Apparently /bin/fish causes some weirdness with nvim.
    /bin/sh should be well tested.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,21 +15,16 @@ function M.setup()
 	vim.o.shiftwidth = 4
 	vim.o.softtabstop = 0
 	vim.o.expandtab = false
-
 	vim.o.nu = true
 	vim.o.signcolumn = "yes"
 	vim.o.relativenumber = true
-
 	vim.o.wrap = false
-
 	vim.o.hlsearch = false
 	vim.o.incsearch = true
-
 	vim.o.scrolloff = 0
-
 	vim.o.updatetime = 50
-
 	vim.o.termguicolors = true
+	vim.o.shell = "/bin/sh"
 end
 
 return M;
